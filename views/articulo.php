af8458524140dd030595bc83168ad3a37ee36226
alapalapa <roberto-750@hotmail.com>
2014-06-07 20:31:43 -0500

    update of title

--- views/articulo.php
+++ views/articulo.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Detalle de Articulo</title>
+	<title>Detalle de Articulos</title>
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<style>
 		th {
